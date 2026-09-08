7f5abe29d16d5f79a899c41d4177b350f65c26d2
Ajit <mac@MacBook-Pro.local>
2026-09-08 21:16:55 +0545

    Add first component First

---
+++ client/src/First.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <>
+    <h2>This is a component.</h2>
+    <div>
+        This is a div.
+    </div>
+    <hr/>
+    <br/>
+    <div>
+        This is another div.
+    </div>
+
+    
+    </>
+  )
+}
+
+export default First
