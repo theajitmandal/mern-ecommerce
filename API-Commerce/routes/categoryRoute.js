490de1ecf950870d05616062e3bb3c61994f94a1
Ajit Mandal <theajitmandal@gmail.com>
2026-09-16 21:43:23 +0545

    Add delete category controllers

--- API-Commerce/routes/categoryRoute.js
+++ API-Commerce/routes/categoryRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { categoryDetails, createCategory, helloFunction, showCategory, updateCategory } from "../controllers/categoryController.js";
+import { categoryDetails, createCategory, deleteCategory, helloFunction, showCategory, updateCategory } from "../controllers/categoryController.js";
 
 router.get("/test", helloFunction);
 router.post("/createcategory", createCategory)
 router.get("/categorylist", showCategory)
 router.get("/categorydetails/:id", categoryDetails)
 router.put("/updatecategory/:id", updateCategory)
+router.delete("/deletecategory/:id", deleteCategory)
 
 export default router;
