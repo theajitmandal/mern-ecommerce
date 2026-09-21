6e68d90b129fdfcd0951d0d030542883ecf9dc89
Ajit Mandal <theajitmandal@gmail.com>
2026-09-21 19:18:18 +0545

    fix  updateProduct controller in api-commerce

--- API-Commerce/routes/productRoute.js
+++ API-Commerce/routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { postProduct, productDetails, showProduct } from "../controllers/productController.js";
+import { postProduct, productDetails, showProduct, updateProduct } from "../controllers/productController.js";
 
 router.post("/postproduct", postProduct)
 router.get("/productlist", showProduct)
 router.get("/productdetails/:id", productDetails)
+router.put("/updateproduct/:id", updateProduct)
 
 export default router;
