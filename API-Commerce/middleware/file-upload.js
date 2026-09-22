0c3cce6e69e8f62605e4d7b1df0e8a3710d9513f
Ajit Mandal <theajitmandal@gmail.com>
2026-09-22 18:37:19 +0545

    Add some more code for file-upload in api/commerce

--- API-Commerce/middleware/file-upload.js
+++ API-Commerce/middleware/file-upload.js
@@ -6,14 +6,33 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         let fileDestination = 'public/uploads'
         // check if directory exists
-        if(!fs.existsSync(fileDestination)){
-            fs.mkdirSync(fileDestination, {recursive: true}) 
+        if (!fs.existsSync(fileDestination)) {
+            fs.mkdirSync(fileDestination, { recursive: true })
             // recursive: true  creates parents folder as well as sub-folder
             cb(null, fileDestination)
         }
-        else{
+        else {
             cb(null, fileDestination)
         }
-    }
+    },
+    filename: (req, file, cb) => {
+        let filename = path.basename(file.originalname, path.extname(file.originalname))
+        // abc.jpg
+        // .jpg
+        // path.basename(abc.jpg, .jpg)
+        // filename will be equal to 'abc'
 
+        let ext = path.extname(file.originalname)
+        // .jpg
+        // file names can be same so changing the original filename
+        cb(null, filename + '_' + Date.now() + ext)
+    }
 })
+
+let imageFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|png|jpeg|svg|jfif|gif||JPG|PNG|JPEG|SVG|JFIF|GIF$)/)) {
+        return cb(new Error('You can upload image file only'), false)
+    } else {
+        cb(null, true)
+    }
+}
